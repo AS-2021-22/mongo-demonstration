189c48f8fb8562a23bbe719c82f7479f62e41c64
HeyJOe <jocarmi03@gmail.com>
2022-03-28 17:50:35 +0200

    DB connected£

--- src/server.js
+++ src/server.js
@@ -1,19 +1,28 @@
 const express           = require('express')
 const bodyParser        = require('body-parser')
-const mongo             = require('mongodb')
+const {MongoClient}    = require('mongodb')
 const path              = require('path')
                           require('dotenv/config')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const htmlFolder = path.join(__dirname,'public','html')
+const htmlFolder = path.join(__dirname,'..','public','html')
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'..','public')))
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@demonstration-mongo.lf3jy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const DB = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+DB.connect(err => {
+    if(err) console.log(err.message)
+    else {
+        app.listen(PORT,() => console.log(`> Server listening on PORT: ${PORT}`))   
+        console.log("DB connected")
+    }
+})
 
 app.get('/',(_,res) => {
     res.sendFile(path.join(htmlFolder,'index.html'))
 })
-
-app.listen(PORT,() => console.log(`> Server listening on PORT: ${PORT}`))
